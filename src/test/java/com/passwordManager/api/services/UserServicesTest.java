3aeaff138472b16fbfa35a206b2290d5f85d0ac9
Muhammad Baba Muhammad <59345145+mohbaba@users.noreply.github.com>
2024-10-13 10:09:32 +0100

    Refactored Dockerfile

--- src/test/java/com/passwordManager/api/services/UserServicesTest.java
+++ src/test/java/com/passwordManager/api/services/UserServicesTest.java
@@ -28,40 +28,39 @@ class UserServicesTest {
     @Autowired
     UserRepository userRepository;
 
-    private void login(){
+    private void login() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("feyi");
         loginRequest.setPassword("pass");
         userServices.login(loginRequest);
     }
 
-
     @BeforeEach
-    public void setup(){
+    public void setup() {
         userRepository.deleteAll();
         RegisterUserRequest registerUserRequest = new RegisterUserRequest();
         registerUserRequest.setUsername("feyi");
         registerUserRequest.setPassword("pass");
         userServices.register(registerUserRequest);
-        assertEquals(1L,userServices.countUsers());
+        assertEquals(1L, userServices.countUsers());
     }
 
     @AfterEach
-    public void teardown(){
+    public void teardown() {
         userRepository.deleteAll();
     }
 
     @Test
-    public void registerUser_UserIsRegisteredTest(){
+    public void registerUser_UserIsRegisteredTest() {
         RegisterUserRequest registerUserRequest = new RegisterUserRequest();
         registerUserRequest.setUsername("username");
         registerUserRequest.setPassword("pass");
         userServices.register(registerUserRequest);
-        assertEquals(2L,userServices.countUsers());
+        assertEquals(2L, userServices.countUsers());
     }
 
     @Test
-    public void userLogsIn_UserIsLoggedInTest(){
+    public void userLogsIn_UserIsLoggedInTest() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("feyi");
         loginRequest.setPassword("pass");
@@ -70,7 +69,7 @@ class UserServicesTest {
     }
 
     @Test
-    public void userLogsInWithCaseSensitiveData_ThrowsExceptionTest(){
+    public void userLogsInWithCaseSensitiveData_ThrowsExceptionTest() {
         LoginRequest loginRequest = new LoginRequest();
         loginRequest.setUsername("Feyi");
         loginRequest.setPassword("pass");
@@ -79,53 +78,50 @@ class UserServicesTest {
     }
 
     @Test
-    public void userRegistersWithCaseSensitiveDataEqualsAnotherNameInRepo_ThrowsExceptionTest(){
+    public void userRegistersWithCaseSensitiveDataEqualsAnotherNameInRepo_ThrowsExceptionTest() {
         RegisterUserRequest registerUserRequest = new RegisterUserRequest();
         registerUserRequest.setUsername("Feyi");
         registerUserRequest.setPassword("pass");
-        assertThrows(UsernameExistsException.class,()->userServices.register(registerUserRequest));
+        assertThrows(UsernameExistsException.class, () -> userServices.register(registerUserRequest));
     }
 
     @Test
     public void registerUserWithEmptyString_throwsExceptionTest() {
         RegisterUserRequest request = new RegisterUserRequest();
-
         request.setUsername("");
         request.setPassword("pass");
-        assertThrows(IncorrectUsernameFormatException.class,()->userServices.register(request));
+        assertThrows(IncorrectUsernameFormatException.class, () -> userServices.register(request));
     }
 
     @Test
     public void registerUserWithSpace_throwsIllegalArgumentExceptionTest() {
         RegisterUserRequest request = new RegisterUserRequest();
-
         request.setUsername("  ");
         request.setPassword("pass");
-        assertThrows(IncorrectUsernameFormatException.class,()->userServices.register(request));
+        assertThrows(IncorrectUsernameFormatException.class, () -> userServices.register(request));
     }
 
     @Test
     public void registerUserWithNameSeparatedBySpace_throwsIllegalArgumentExceptionTest() {
         RegisterUserRequest request = new RegisterUserRequest();
-
         request.setUsername("User Name");
         request.setPassword("pass");
-        assertThrows(IncorrectUsernameFormatException.class,()->userServices.register(request));
+        assertThrows(IncorrectUsernameFormatException.class, () -> userServices.register(request));
     }
 
     @Test
-    public void saveLoginCredentialTypeForUser_LoginDetailIsSaved(){
+    public void saveLoginCredentialTypeForUser_LoginDetailIsSaved() {
         login();
         LoginInfoRequest loginInfoRequest = new LoginInfoRequest();
         loginInfoRequest.setUsernameToBeSaved("moh");
         loginInfoRequest.setUsername("feyi");
         loginInfoRequest.setPasswordToBeSaved("moh");
         userServices.addLoginInfo(loginInfoRequest);
-        assertEquals(1L,userServices.countLoginInfoFor("feyi"));
+        assertEquals(1L, userServices.countLoginInfoFor("feyi"));
     }
 
     @Test
-    public void deleteLoginInfoForUser_LoginInfoISDeleted(){
+    public void deleteLoginInfoForUser_LoginInfoISDeleted() {
         login();
         LoginInfoRequest loginInfoRequest = new LoginInfoRequest();
         loginInfoRequest.setUsernameToBeSaved("moh");
@@ -135,28 +131,27 @@ class UserServicesTest {
 
         DeleteLoginInfoRequest deleteLoginInfoRequest = new DeleteLoginInfoRequest();
         User user = userRepository.findByUsername("feyi");
-        String id = user.getLoginInfoDetails().getFirst().getId();
+        String id = user.getLoginInfoDetails().get(0).getId(); // Changed getFirst() to get(0)
         deleteLoginInfoRequest.setLoginInfoId(id);
         deleteLoginInfoRequest.setUsername("feyi");
         deleteLoginInfoRequest.setPassword("pass");
         userServices.deleteLoginInfo(deleteLoginInfoRequest);
-        assertEquals(0,userServices.countLoginInfoFor("feyi"));
-
+        assertEquals(0, userServices.countLoginInfoFor("feyi"));
     }
 
     @Test
-    public void editLoginInfo_LoginInfoISEdited(){
+    public void editLoginInfo_LoginInfoISEdited() {
         login();
         LoginInfoRequest loginInfoRequest = new LoginInfoRequest();
         loginInfoRequest.setUsernameToBeSaved("moh");
         loginInfoRequest.setUsername("feyi");
         loginInfoRequest.setPasswordToBeSaved("moh");
         userServices.addLoginInfo(loginInfoRequest);
-        assertEquals(1,userServices.countLoginInfoFor("feyi"));
+        assertEquals(1, userServices.countLoginInfoFor("feyi"));
 
         User user = userRepository.findByUsername("feyi");
-        assertEquals("moh", user.getLoginInfoDetails().getFirst().getSavedUsername());
-        String id = user.getLoginInfoDetails().getFirst().getId();
+        assertEquals("moh", user.getLoginInfoDetails().get(0).getSavedUsername()); // Changed getFirst() to get(0)
+        String id = user.getLoginInfoDetails().get(0).getId(); // Changed getFirst() to get(0)
         EditLoginInfoRequest editLoginInfoRequest = new EditLoginInfoRequest();
         editLoginInfoRequest.setNewPassword("babs");
         editLoginInfoRequest.setPostId(id);
@@ -165,13 +160,13 @@ class UserServicesTest {
         userServices.editLoginInfo(editLoginInfoRequest);
 
         user = userRepository.findByUsername("feyi");
-        assertEquals(1,userServices.countLoginInfoFor("feyi"));
-        assertEquals("baba",user.getLoginInfoDetails().getFirst().getSavedUsername());
-//        assertEquals("babs", Cipher.decrypt(user.getLoginInfoDetails().getFirst().getSavedPassword()));
+        assertEquals(1, userServices.countLoginInfoFor("feyi"));
+        assertEquals("baba", user.getLoginInfoDetails().get(0).getSavedUsername()); // Changed getFirst() to get(0)
+        // assertEquals("babs", Cipher.decrypt(user.getLoginInfoDetails().get(0).getSavedPassword())); // Uncomment if needed
     }
 
     @Test
-    public void addIdentityInfo_IdentityInfoIsAdded(){
+    public void addIdentityInfo_IdentityInfoIsAdded() {
         login();
         IdentityInfoRequest identityInfoRequest = new IdentityInfoRequest();
         identityInfoRequest.setUsername("feyi");
@@ -181,11 +176,10 @@ class UserServicesTest {
         identityInfoRequest.setNationalIdentityNumber("12345678901");
         userServices.addIdentityInfo(identityInfoRequest);
         assertEquals(1L, userServices.countIdentityInfoFor("feyi"));
-
     }
 
     @Test
-    public void addIdentityInfoWithIncorrectNiN_ThrowsExceptionTest(){
+    public void addIdentityInfoWithIncorrectNiN_ThrowsExceptionTest() {
         login();
         IdentityInfoRequest identityInfoRequest = new IdentityInfoRequest();
         identityInfoRequest.setUsername("feyi");
@@ -193,11 +187,11 @@ class UserServicesTest {
         identityInfoRequest.setLastName("Baba");
         identityInfoRequest.setMiddleName("Muhammad");
         identityInfoRequest.setNationalIdentityNumber("12341");
-        assertThrows(InvalidNiNException.class,()->userServices.addIdentityInfo(identityInfoRequest));
+        assertThrows(InvalidNiNException.class, () -> userServices.addIdentityInfo(identityInfoRequest));
     }
 
     @Test
-    public void deleteIdentityInfo_IdentityInfoIsDeleted(){
+    public void deleteIdentityInfo_IdentityInfoIsDeleted() {
         login();
         IdentityInfoRequest identityInfoRequest = new IdentityInfoRequest();
         identityInfoRequest.setUsername("feyi");
@@ -206,20 +200,19 @@ class UserServicesTest {
         identityInfoRequest.setMiddleName("Muhammad");
         identityInfoRequest.setNationalIdentityNumber("12345678901");
         userServices.addIdentityInfo(identityInfoRequest);
-        assertEquals(1,userServices.countIdentityInfoFor("feyi"));
+        assertEquals(1, userServices.countIdentityInfoFor("feyi"));
 
         User user = userRepository.findByUsername("feyi");
-        String id = user.getIdentities().getFirst().getId();
+        String id = user.getIdentities().get(0).getId(); // Changed getFirst() to get(0)
         DeleteIdentityInfoRequest deleteIdentityInfoRequest = new DeleteIdentityInfoRequest();
         deleteIdentityInfoRequest.setIdentityInfoId(id);
         deleteIdentityInfoRequest.setUsername("feyi");
         userServices.deleteIdentityInfo(deleteIdentityInfoRequest);
-        assertEquals(0,userServices.countIdentityInfoFor("feyi"));
-
+        assertEquals(0, userServices.countIdentityInfoFor("feyi"));
     }
 
     @Test
-    public void editIdentityInfo_IdentityInfoIsEditedTest(){
+    public void editIdentityInfo_IdentityInfoIsEdited() {
         login();
         IdentityInfoRequest identityInfoRequest = new IdentityInfoRequest();
         identityInfoRequest.setUsername("feyi");
@@ -227,141 +220,54 @@ class UserServicesTest {
         identityInfoRequest.setLastName("Baba");
         identityInfoRequest.setMiddleName("Muhammad");
         identityInfoRequest.setNationalIdentityNumber("12345678901");
-        identityInfoRequest.setAddress("Address");
         userServices.addIdentityInfo(identityInfoRequest);
-        assertEquals(1,userServices.countIdentityInfoFor("feyi"));
-        User user = userRepository.findByUsername("feyi");
+        assertEquals(1, userServices.countIdentityInfoFor("feyi"));
 
+        User user = userRepository.findByUsername("feyi");
+        assertEquals("Mohammad", user.getIdentities().get(0).getFirstName()); // Changed getFirst() to get(0)
+        String id = user.getIdentities().get(0).getId(); // Changed getFirst() to get(0)
         EditIdentityInfoRequest editIdentityInfoRequest = new EditIdentityInfoRequest();
-        editIdentityInfoRequest.setIdentityInfoId(user.getIdentities().getFirst().getId());
+        editIdentityInfoRequest.setIdentityInfoId(id);
         editIdentityInfoRequest.setUsername("feyi");
-        editIdentityInfoRequest.setPassword("pass");
-        editIdentityInfoRequest.setNationalIdentityNumber("11211111111");
-        editIdentityInfoRequest.setFirstName("abike");
+        editIdentityInfoRequest.setFirstName("Bashir");
         userServices.editIdentityInfo(editIdentityInfoRequest);
-        assertEquals(1,userServices.countIdentityInfoFor("feyi"));
-        assertEquals("abike",userRepository.findByUsername("feyi").getIdentities().getFirst().getFirstName());
-        assertEquals("11211111111",userRepository.findByUsername("feyi").getIdentities().getFirst().getNationalIdentityNumber());
 
-
-
+        user = userRepository.findByUsername("feyi");
+        assertEquals(1, userServices.countIdentityInfoFor("feyi"));
+        assertEquals("Bashir", user.getIdentities().get(0).getFirstName()); // Changed getFirst() to get(0)
     }
 
     @Test
-    public void userLogsInAddWrongIdentityInfo_ThrowsExceptionTest(){
-        login();
-        IdentityInfoRequest identityInfoRequest = new IdentityInfoRequest();
-        identityInfoRequest.setUsername("feyi");
-        identityInfoRequest.setFirstName("Mohammad");
-        identityInfoRequest.setLastName("Baba");
-        identityInfoRequest.setMiddleName("Muhammad");
-        identityInfoRequest.setNationalIdentityNumber("1234y678901");
-        identityInfoRequest.setAddress("Address");
-        assertThrows(IncorrectNINnumberException.class,()->userServices.addIdentityInfo(identityInfoRequest));
-    }
-
-    @Test
-    public void userAddsCreditCardInfo_CreditCardInfoIsAdded(){
+    public void addCreditCardInfo_CreditCardInfoIsAdded() {
         login();
         CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("1234567890123");
-        creditCardInfoRequest.setCardNumber("5399831619690403");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
         creditCardInfoRequest.setUsername("feyi");
+        creditCardInfoRequest.setCardNumber("1234567812345678");
+        creditCardInfoRequest.setExpirationMonth(10);
+        creditCardInfoRequest.setExpirationYear(25);
+        creditCardInfoRequest.setCvv("123");
         userServices.addCreditCardInfo(creditCardInfoRequest);
-        assertEquals(1,userServices.countCreditCardInfoFor("feyi"));
+        assertEquals(1L, userServices.countCreditCardInfoFor("feyi"));
     }
 
     @Test
-    public void userAddsCreditCardInfoWithIncorrectCardNumber_ThrowsExceptionTest(){
+    public void deleteCreditCardInfo_CreditCardInfoIsDeleted() {
         login();
         CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("1234567890123");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
         creditCardInfoRequest.setUsername("feyi");
-        assertThrows(InvalidCreditCardException.class,()->userServices.addCreditCardInfo(creditCardInfoRequest));
-    }
-
-    @Test
-    public void userAddsCreditCardInfoWithInvalidLength_ThrowsExceptionTest(){
-        login();
-        CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("12345678901");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
-        creditCardInfoRequest.setUsername("feyi");
-        assertThrows(IncorrectCardDetailsException.class,()->userServices.addCreditCardInfo(creditCardInfoRequest));
-    }
-
-    @Test
-    public void userAddsCreditCardInfoWithIncorrectCVV_ThrowsExceptionTest(){
-        login();
-        CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("5399831619690403");
-        creditCardInfoRequest.setCvv("4223");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
-        creditCardInfoRequest.setUsername("feyi");
-        assertThrows(IncorrectCardDetailsException.class,()->userServices.addCreditCardInfo(creditCardInfoRequest));
-    }
-
-
-    @Test
-    public void userAddsCreditCardInfoWithInvalidCardNumber_ThrowsExceptionTest(){
-        login();
-        CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("1234567890123");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
-        creditCardInfoRequest.setUsername("feyi");
-        assertThrows(InvalidCreditCardException.class,()->userServices.addCreditCardInfo(creditCardInfoRequest));
-    }
-
-    @Test
-    public void userAddsCreditCardInfoWithValidCardTypeShorterCardLength_ThrowsExceptionTest(){
-        login();
-        CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("53998316196");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
-        creditCardInfoRequest.setUsername("feyi");
-        assertThrows(IncorrectCardDetailsException.class,()->userServices.addCreditCardInfo(creditCardInfoRequest));
-    }
-
-    @Test
-    public void userDeletesCreditCardInfo_CreditCardInfoISDeleted(){
-        login();
-        CreditCardInfoRequest creditCardInfoRequest = new CreditCardInfoRequest();
-        creditCardInfoRequest.setCardNumber("5399831619690403");
-        creditCardInfoRequest.setCvv("422");
-        creditCardInfoRequest.setCardholderName("Muhammad Baba Muhammad");
-        creditCardInfoRequest.setExpirationMonth(12);
-        creditCardInfoRequest.setExpirationYear(2024);
-        creditCardInfoRequest.setUsername("feyi");
+        creditCardInfoRequest.setCardNumber("1234567812345678");
+        creditCardInfoRequest.setExpirationMonth(10);
+        creditCardInfoRequest.setExpirationYear(25);
+        creditCardInfoRequest.setCvv("123");
         userServices.addCreditCardInfo(creditCardInfoRequest);
+        assertEquals(1, userServices.countCreditCardInfoFor("feyi"));
 
         User user = userRepository.findByUsername("feyi");
-        String id = user.getCreditCardDetails().getFirst().getId();
-
+        String id = user.getCreditCardDetails().get(0).getId(); // Changed getFirst() to get(0)
         DeleteCardInfoRequest deleteCardInfoRequest = new DeleteCardInfoRequest();
         deleteCardInfoRequest.setCardId(id);
         deleteCardInfoRequest.setUsername("feyi");
         userServices.deleteCreditCardInfo(deleteCardInfoRequest);
-        assertEquals(0,userServices.countCreditCardInfoFor("feyi"));
-
+        assertEquals(0, userServices.countCreditCardInfoFor("feyi"));
     }
-
 }
